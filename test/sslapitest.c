fa5ea4b15c3dad059a4ffd9fc29351fb02ced801
Frank Denis <github@pureftpd.org>
2024-11-14 01:40:19 +0100

    Add support for the TLS_AEGIS_128{L,X2}_SHA256 cipher suites
    Assumes that CPU support for AES is available on x86_64 and aarch64.
    
    Implementations are derived from libaegis.

--- test/sslapitest.c
+++ test/sslapitest.c
@@ -4593,7 +4593,15 @@ static const char *ciphersuites[] = {
 #endif
 #if !defined(OPENSSL_NO_INTEGRITY_ONLY_CIPHERS)
     "tls_sha256_sha256",
-    "tls_sha384_sha384"
+    "tls_sha384_sha384",
+#else
+    NULL, NULL,
+#endif
+#ifndef OPENSSL_NO_AEGIS
+    "TLS_AEGIS_128L_SHA256",
+    "TLS_AEGIS_128X2_SHA256",
+#else
+    NULL, NULL,
 #endif
 };
 
@@ -5096,7 +5104,7 @@ end:
 }
 
 /*
- * Test TLSv1.3 PSK can be used to send early_data with all 7 ciphersuites
+ * Test TLSv1.3 PSK can be used to send early_data with all 8 ciphersuites
  * idx == 0: Test with TLS1_3_RFC_AES_128_GCM_SHA256
  * idx == 1: Test with TLS1_3_RFC_AES_256_GCM_SHA384
  * idx == 2: Test with TLS1_3_RFC_CHACHA20_POLY1305_SHA256,
@@ -5104,6 +5112,8 @@ end:
  * idx == 4: Test with TLS1_3_RFC_AES_128_CCM_8_SHA256
  * idx == 5: Test with TLS1_3_RFC_SHA256_SHA256
  * idx == 6: Test with TLS1_3_RFC_SHA384_SHA384
+ * idx == 7: Test with TLS1_3_RFC_AEGIS_128L_SHA256
+ * idx == 8: Test with TLS1_3_RFC_AEGIS_128X2_SHA256
  */
 static int test_early_data_psk_with_all_ciphers(int idx)
 {
@@ -5127,10 +5137,16 @@ static int test_early_data_psk_with_all_ciphers(int idx)
         TLS1_3_RFC_AES_128_CCM_8_SHA256,
 #if !defined(OPENSSL_NO_INTEGRITY_ONLY_CIPHERS)
         TLS1_3_RFC_SHA256_SHA256,
-        TLS1_3_RFC_SHA384_SHA384
+        TLS1_3_RFC_SHA384_SHA384,
 #else
         NULL,
-        NULL
+        NULL,
+#endif
+#ifndef OPENSSL_NO_AEGIS
+        TLS1_3_RFC_AEGIS_128L_SHA256,
+        TLS1_3_RFC_AEGIS_128X2_SHA256,
+#else
+        NULL,
 #endif
     };
     const unsigned char *cipher_bytes[] = {
@@ -5145,10 +5161,16 @@ static int test_early_data_psk_with_all_ciphers(int idx)
         TLS13_AES_128_CCM_8_SHA256_BYTES,
 #if !defined(OPENSSL_NO_INTEGRITY_ONLY_CIPHERS)
         TLS13_SHA256_SHA256_BYTES,
-        TLS13_SHA384_SHA384_BYTES
+        TLS13_SHA384_SHA384_BYTES,
 #else
         NULL,
-        NULL
+        NULL,
+#endif
+#ifndef OPENSSL_NO_AEGIS
+        TLS13_AEGIS_128L_SHA256_BYTES,
+        TLS13_AEGIS_128X2_SHA256_BYTES,
+#else
+        NULL,
 #endif
     };
 
@@ -6131,7 +6153,11 @@ static int test_tls13_ciphersuite(int idx)
 #if !defined(OPENSSL_NO_INTEGRITY_ONLY_CIPHERS)
         /* Integrity-only cipher do not provide any confidentiality */
         { TLS1_3_RFC_SHA256_SHA256, 0, 1 },
-        { TLS1_3_RFC_SHA384_SHA384, 0, 1 }
+        { TLS1_3_RFC_SHA384_SHA384, 0, 1 },
+#endif
+#ifndef OPENSSL_NO_AEGIS
+        { TLS1_3_RFC_AEGIS_128L_SHA256, 0, 0 },
+        { TLS1_3_RFC_AEGIS_128X2_SHA256, 0, 0 },
 #endif
     };
     const char *t13_cipher = NULL;
@@ -9027,8 +9053,14 @@ static struct {
         "AES256-SHA:AES128-SHA256",
         NULL,
         "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:"
-        "TLS_AES_128_GCM_SHA256:AES256-SHA",
-        "TLS_AES_256_GCM_SHA384:TLS_AES_128_GCM_SHA256:AES256-SHA" },
+        "TLS_AES_128_GCM_SHA256:"
+#ifndef OPENSSL_NO_AEGIS
+        "TLS_AEGIS_128L_SHA256:"
+        "TLS_AEGIS_128X2_SHA256:"
+#endif
+        "AES256-SHA",
+        "TLS_AES_256_GCM_SHA384:TLS_AES_128_GCM_SHA256:"
+        "AES256-SHA" },
 #endif
 #ifndef OSSL_NO_USABLE_TLS1_3
     { TLS1_3_VERSION,
